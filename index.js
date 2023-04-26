89e1d288d985624cdb9f18b8348182d74ac89467
Albinpirela <119135868+Albinpirela@users.noreply.github.com>
2023-04-26 12:34:05 -0400

    Add close button into the modal of menu

--- index.js
+++ index.js
@@ -4,24 +4,31 @@ const skillsLink = document.querySelector('.skills-link');
 const worksLink = document.querySelector('.works-link');
 const contactLink = document.querySelector('.contact-link');
 const menu = document.querySelector('.hamburger-menu');
+const closeMenu = document.createElement('span');
+
+// Add styles to the clouse menu
+
+closeMenu.innerHTML = 'X';
+menu.appendChild(closeMenu);
+closeMenu.classList.add('clouse-menu');
+closeMenu.style.position = 'absolute';
+closeMenu.style.top = '10px';
+closeMenu.style.right = '40px';
+closeMenu.style.fontSize = '1.5rem';
+closeMenu.style.cursor = 'pointer';
+closeMenu.style.padding = '5px';
+closeMenu.style.border = '1px solid #ff8303';
+closeMenu.style.borderRadius = '50%';
 
 function hiddenMenu() {
   menu.classList.add('active');
   document.body.classList.remove('opacity');
 }
 
-function toggleMenu(e) {
+function toggleMenu() {
 // Change the styles for the menu and the body
   menu.classList.toggle('active');
   document.body.classList.toggle('opacity');
-
-  // Change the "src" attribute of the menu icon
-  const currentIcon = e.target.querySelector('svg');
-  if (currentIcon.classList.contains('fa-solid')) {
-    currentIcon.setAttribute('class', 'fa-sharp');
-  } else {
-    currentIcon.setAttribute('class', 'fa-solid fa-bars');
-  }
 }
 
 // Event listeners
@@ -30,3 +37,4 @@ skillsLink.addEventListener('click', hiddenMenu);
 worksLink.addEventListener('click', hiddenMenu);
 contactLink.addEventListener('click', hiddenMenu);
 faMenu.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', hiddenMenu);
